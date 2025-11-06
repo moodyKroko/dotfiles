7f2152a13584b0619b20c4c1924bef20039feb03
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-11-06 19:10:53 +0000

    add alternate code to launch waybar

--- .local/bin/scripts/launch_waybar.sh
+++ .local/bin/scripts/launch_waybar.sh
@@ -7,3 +7,17 @@ if [[ $USER = "moody" ]] then
 else
   waybar &
 fi
+
+# enable later
+# CONFIG='dir/config/jsonc'
+# STYLE='dir/style.css'
+
+# if pgrep -x "waybar" > /dev/null; then
+#   killall -SIGUSR2 waybar
+# else
+#   if [[ $USER = "moody" ]] then
+#     waybar -c "$CONFIG" -s "$STYLE" > /dev/null 2>&1 &
+#   else
+#     waybar /dev/null 2>&1 &
+#   fi
+# fi
