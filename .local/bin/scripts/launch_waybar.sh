8fdadd7a7f7618e197b39d488bc7cc78e11f73d0
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-11-13 20:36:07 +0000

    update waybar launch script

--- .local/bin/scripts/launch_waybar.sh
+++ .local/bin/scripts/launch_waybar.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-killall waybar
-
-if [[ $USER = "moody" ]] then
-  waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
-else
-  waybar &
-fi
-
 # enable later
-# CONFIG='dir/config/jsonc'
-# STYLE='dir/style.css'
+CONFIG=~/.config/waybar/config.jsonc
+STYLE=~/.config/waybar/style.css
 
-# if pgrep -x "waybar" > /dev/null; then
-#   killall -SIGUSR2 waybar
-# else
-#   if [[ $USER = "moody" ]] then
-#     waybar -c "$CONFIG" -s "$STYLE" > /dev/null 2>&1 &
-#   else
-#     waybar /dev/null 2>&1 &
-#   fi
-# fi
+if pgrep -x "waybar" > /dev/null; then
+  killall -SIGUSR2 waybar
+else
+  if [[ $USER = "moody" ]] then
+    waybar -c $CONFIG -s $STYLE > /dev/null 2>&1 &
+  else
+    waybar /dev/null 2>&1 &
+  fi
+fi
