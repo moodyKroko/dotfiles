d62f87f40789518765a314a4ecfc222125117bac
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-12-01 21:47:05 +0000

    update: remove user check

--- .local/bin/scripts/launch_waybar.sh
+++ .local/bin/scripts/launch_waybar.sh
@@ -7,9 +7,5 @@ STYLE=~/.config/waybar/style.css
 if pgrep -x "waybar" > /dev/null; then
   killall -SIGUSR2 waybar
 else
-  if [[ $USER = "moody" ]] then
     waybar -c $CONFIG -s $STYLE > /dev/null 2>&1 &
-  else
-    waybar /dev/null 2>&1 &
-  fi
 fi
