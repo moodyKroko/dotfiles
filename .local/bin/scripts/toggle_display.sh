d14c9b6f39385dc4171a38b1af3bf1c6da8e25cf
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-12-08 22:24:26 +0000

    update toggle display to check for hyprland or i3wm

--- .local/bin/scripts/toggle_display.sh
+++ .local/bin/scripts/toggle_display.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 # Get the names of your displays
-INTERNAL="eDP-1"  # Typically the internal laptop display
-EXTERNAL="HDMI-1" # Your external display, adjust as needed
+INTERNAL="eDP-1"    # Typically the internal laptop display
+EXTERNAL="HDMI-A-1" # Your external display, adjust as needed
+DEFAULT_ENABLE="$INTERNAL, 1920x1080@60, 0x0, 1"
 
-# Check if the external display is connected
-if xrandr | grep "$EXTERNAL connected"; then
-  # If external is active, switch to internal only
-  # if xrandr | grep "$EXTERNAL connected primary"; then
-  #   xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto --primary
-  # # If internal is active, switch to external only
-  # else
-  #   xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto --primary
-  # fi
-  xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto --primary --mode 1920x1080 --rate 74.99
-else
-  # If no external display, ensure internal is on
-  xrandr --output "$INTERNAL" --auto --primary
+if printenv | grep -q "HYPRLAND_INSTANCE_SIGNATURE"; then
+  if hyprctl monitors | grep -q $EXTERNAL; then
+    hyprctl keyword monitor "$INTERNAL,disable"
+  else
+    hyprctl keyword monitor "$DEFAULT_ENABLE"
+  fi
+  exit 0
 fi
 
-# Restart i3 to apply changes
-i3-msg restart
+if pgrep -x "i3" >/dev/null; then
+  # Check if the external display is connected
+  if xrandr | grep "$EXTERNAL connected"; then
+    xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto --primary --mode 1920x1080 --rate 74.99
+  else
+    # If no external display, ensure internal is on
+    xrandr --output "$INTERNAL" --auto --primary
+  fi
+
+  # Restart i3 to apply changes
+  i3-msg restart
+fi
