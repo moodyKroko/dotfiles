1a5ff800f35a1380aea1c43430a6c010baaca967
moodyKroko <59341541+moodyKroko@users.noreply.github.com>
2025-12-08 23:43:50 +0000

    update: toggle_display for better logic and readability

--- .local/bin/scripts/toggle_display.sh
+++ .local/bin/scripts/toggle_display.sh
@@ -3,25 +3,40 @@
 # Get the names of your displays
 INTERNAL="eDP-1"    # Typically the internal laptop display
 EXTERNAL="HDMI-A-1" # Your external display, adjust as needed
-DEFAULT_ENABLE="$INTERNAL, 1920x1080@60, 0x0, 1"
+DEFAULT_ENABLE="${INTERNAL}, 1920x1080@60, 0x0, 1"
 
-if printenv | grep -q "HYPRLAND_INSTANCE_SIGNATURE"; then
-  if hyprctl monitors | grep -q $EXTERNAL; then
-    hyprctl keyword monitor "$INTERNAL,disable"
-  else
-    hyprctl keyword monitor "$DEFAULT_ENABLE"
+if [[ -n "${HYPRLAND_INSTANCE_SIGNATURE:-}" ]]; then
+
+  if ! command -v jq &>/dev/null; then
+    notify-send -u critical "Error: jq is required but not installed."
+    exit 1
   fi
+
+  connected_monitors=$(hyprctl monitors all -j | jq -r '.[].name')
+
+  case "${connected_monitors}" in
+  *"${EXTERNAL}"*)
+    hyprctl keyword monitor "${INTERNAL},disable"
+    ;;
+  *)
+    hyprctl keyword monitor "${DEFAULT_ENABLE}"
+    ;;
+  esac
   exit 0
 fi
 
 if pgrep -x "i3" >/dev/null; then
-  # Check if the external display is connected
-  if xrandr | grep "$EXTERNAL connected"; then
-    xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto --primary --mode 1920x1080 --rate 74.99
-  else
-    # If no external display, ensure internal is on
-    xrandr --output "$INTERNAL" --auto --primary
-  fi
+  connected_x_monitors=$(xrandr --query | awk '/ connected/ {print $1}')
+
+  case "${connected_x_monitors}" in
+  *"${EXTERNAL}"*)
+    xrandr --output "${INTERNAL}" --off \
+      --output "${EXTERNAL}" --auto --primary --mode 1920x1080 --rate 74.99
+    ;;
+  *)
+    xrandr --output "${INTERNAL}" --auto --primary
+    ;;
+  esac
 
   # Restart i3 to apply changes
   i3-msg restart
